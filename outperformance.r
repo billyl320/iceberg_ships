9e525743e35d75f8a018cb8c9c2359186c735813
William Franz Lamberti <wlamber2@gmu.edu>
2020-04-10 11:47:39 -0400

    Add files via upload
    outperformance measurement

---
+++ outperformance.r
@@ -0,0 +1,19 @@
+#outperformance rate calculation for RF over CNN
+#CNNs from Li et.al, 2019
+
+#testing
+cnn_acc<-c(91.5, 93.72, 93.5, 94.9)
+
+rf_acc<-c(rep(100, 4))
+
+mean(rf_acc/cnn_acc)-1
+
+#validation
+cnn_acc<-c(86.51, 87.72, 84.83, 87.02)
+
+rf_acc<-c(rep(100, 4))
+
+mean(rf_acc/cnn_acc)-1
+
+
+#
